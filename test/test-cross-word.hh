58a8048ab10be1dc63de9bb089d7fa2e807a2e48
chadrc <chadrcollins91@gmail.com>
2017-05-28 22:22:09 -0400

    Setup test for multi join.

--- test/test-cross-word.hh
+++ test/test-cross-word.hh
@@ -1,34 +1,38 @@
 <?hh
 require_once("shared/cross-word.hh");
 
-$word_hint_map = Map {
-  "myWord" => "First Word",
-  "myOtherWord" => "Another Word",
-  "dogs" => "Furry animal.",
-  "dragons" => "Mythical creature."
-};
-
-$cross_word = null;
-try {
-  $cross_word = create_cross_word($word_hint_map);
-} catch (CrossWordCreationFailed $e) {
-  foreach ($e->get_attempts() as $attempt) {
-    echo $attempt;
-  }
+function test_basic() {
+  get_cross_word(Map {
+    "myWord" => "First Word",
+    "myOtherWord" => "Another Word",
+    "dogs" => "Furry animal.",
+    "dragons" => "Mythical creature."
+  });
 }
 
-if ($cross_word !== null) {
-  echo $cross_word->get_grid();
-  foreach ($cross_word->get_grid() as $index => $col) {
-    foreach ($col as $index => $cell) {
-      $x = $cell->get_x();
-      $y = $cell->get_y();
-      $letter = $cell->get_letter();
-      $join = "";
-      if ($cell->get_join() !== null) {
-        $join = " | (" . $cell->get_join()->get_x() . ", " . $cell->get_join()->get_y() . ") - " . $cell->get_join()->get_letter();
-      }
-      echo "(" . $x . ", " . $y . ") - " . $letter . $join . "\n";
+function test_multi_join() {
+  get_cross_word(Map {
+    "oooowooo" => "First Word",
+    "ooooxooo" => "Another Word",
+    "oooohooo" => "Furry animal.",
+    "ooookooo" => "Mythical creature."
+  });
+}
+
+function get_cross_word(Map<string, string> $word_hint_map) {
+  $cross_word = null;
+  try {
+    $cross_word = create_cross_word($word_hint_map);
+  } catch (CrossWordCreationFailed $e) {
+    foreach ($e->get_attempts() as $attempt) {
+      echo $attempt;
     }
   }
+
+  if ($cross_word !== null) {
+    echo $cross_word->get_grid();
+  }
 }
+
+// test_basic();
+test_multi_join();
