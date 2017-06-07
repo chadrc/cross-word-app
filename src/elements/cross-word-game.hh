1111090aaf088dda8628ee0e0d52c91827523696
chadrc <chadrcollins91@gmail.com>
2017-06-06 23:24:16 -0400

    Made all attributes required for cross word game element.

--- src/elements/cross-word-game.hh
+++ src/elements/cross-word-game.hh
@@ -5,29 +5,21 @@ class :cross-word-game extends :x:element {
   use XHPReact;
 
   attribute :xhp:html-element;
-  attribute Vector<CrossWordString> horizontal = Vector {};
-  attribute Vector<CrossWordString> vertical = Vector {};
-  attribute CrossWordGrid grid;
+  attribute Vector<CrossWordString> horizontal @required;
+  attribute Vector<CrossWordString> vertical @required;
+  attribute CrossWordGrid grid @required;
 
   protected function render(): XHPRoot {
     $blank_grid = Map {};
-    $grid = $this->:grid;
-    $lim = Map {};
-    if ($grid !== null) {
-      $limits = $grid->get_limits();
-      for ($x=$limits->max_y(); $x>=$limits->min_y(); $x--) {
-        $blank_grid[$x] = Map {};
-        for ($y=$limits->min_x(); $y<=$limits->max_x(); $y++) {
-          $cell = $grid->get_cell($y, $x);
-          if ($cell !== null) {
-            $blank_grid[$x][$y] = true;
-          }
+    $limits = $this->:grid->get_limits();
+    for ($x=$limits->max_y(); $x>=$limits->min_y(); $x--) {
+      $blank_grid[$x] = Map {};
+      for ($y=$limits->min_x(); $y<=$limits->max_x(); $y++) {
+        $cell = $this->:grid->get_cell($y, $x);
+        if ($cell !== null) {
+          $blank_grid[$x][$y] = true;
         }
       }
-      $lim['minX'] = $limits->min_x();
-      $lim['maxX'] = $limits->max_x();
-      $lim['minY'] = $limits->min_y();
-      $lim['maxY'] = $limits->max_y();
     }
     $horz_hints = $this->:horizontal->map(
       $value ==> $value->get_hint()
@@ -41,7 +33,12 @@ class :cross-word-game extends :x:element {
         'horizontal' => $horz_hints,
         'vertical' => $vert_hints,
         'grid' => $blank_grid,
-        'limits' => $lim
+        'limits' => [
+          'minX' => $limits->min_x(),
+          'maxX' => $limits->max_x(),
+          'minY' => $limits->min_y(),
+          'maxY' => $limits->max_y()
+        ]
       },
     );
 
