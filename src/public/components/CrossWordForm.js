3e9e3bba9d504e5a95dd96d64247e3d9f7228905
chadrc <chadrcollins91@gmail.com>
2017-06-11 13:31:59 -0400

    Implemented restart and view buttons after successful puzzle creation.

--- src/public/components/CrossWordForm.js
+++ src/public/components/CrossWordForm.js
@@ -1,11 +1,37 @@
 class CrossWordForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      title: "",
-      words: [],
-      forceFocusIndex: null
-    };
+    this.state = this.defaultState;
+  }
+
+  get defaultState() {
+    let words = [];
+    let title = "";
+
+    if (localStorage.puzzleDraft) {
+      let store = JSON.parse(localStorage.puzzleDraft);
+      words = store.words;
+      title = store.title;
+    } else {
+      localStorage.puzzleDraft = JSON.stringify({
+        title: "",
+        words: []
+      });
+    }
+
+    return {
+      title: title,
+      words: words,
+      forceFocusIndex: null,
+      createdPuzzleId: ""
+    }
+  }
+
+  storeState() {
+    localStorage.puzzleDraft = JSON.stringify({
+      title: this.state.title,
+      words: this.state.words
+    });
   }
 
   addWord() {
@@ -32,18 +58,24 @@ class CrossWordForm extends React.Component {
     });
   }
 
+  titleChanged(value) {
+    this.setState({
+      title: value
+    }, () => this.storeState());
+  }
+
   wordChanged(index, value) {
     this.state.words[index].word = value;
     this.setState({
       words: this.state.words
-    });
+    }, () => this.storeState());
   }
 
   hintChanged(index, value) {
     this.state.words[index].hint = value;
     this.setState({
       words: this.state.words
-    });
+    }, () => this.storeState());
   }
 
   submit(e) {
@@ -59,21 +91,44 @@ class CrossWordForm extends React.Component {
     }).then((response) => {
       return response.json()
     }).then((obj) => {
-      console.log("resposne: ", obj);
+      this.setState({
+        createdPuzzleId: obj.puzzleId
+      })
     });
   }
 
+  restart() {
+    localStorage.puzzleDraft = "";
+    this.setState(this.defaultState);
+  }
+
   render() {
+    if (this.state.createdPuzzleId) {
+      return (
+        <section className="puzzle-created-prompt">
+          <h2>Puzzle Created</h2>
+          <a href={`/puzzle/${this.state.createdPuzzleId}`}>View</a>
+          <button type="button" onClick={() => this.restart()}>Make Another</button>
+        </section>
+      );
+    }
+
     return (
       <form onSubmit={(e) => this.submit(e)}>
         <section>
           <label htmlFor="title">Title</label>
-          <input tabIndex={1} type="text" id="title" />
+          <input  tabIndex={1}
+                  type="text"
+                  id="title"
+                  value={this.state.title}
+                  onChange={(e) => this.titleChanged(e.target.value)} />
         </section>
         <section>
           <label>Words</label>
           {this.state.words.map((item, index) => {
             return <WordInput key={index}
+                              word={item.word}
+                              hint={item.hint}
                               onFocus={() => this.wordFocused()}
                               onWordChange={(value) => this.wordChanged(index, value)}
                               onHintChange={(value) => this.hintChanged(index, value)}
@@ -131,11 +186,13 @@ class WordInput extends React.Component {
         <input  ref={(input) => this.wordInput = input}
                 tabIndex={this.props.tabIndex}
                 type="text"
+                value={this.props.word}
                 onFocus={() => this.raiseOnFocused("word")}
                 onChange={(e) => this.raiseWordChanged(e.target.value)} />
         <textarea ref={(input) => this.hintInput = input}
                   tabIndex={this.props.tabIndex + 1}
                   rows={2} type="text"
+                  value={this.props.hint}
                   onFocus={() => this.raiseOnFocused("hint")}
                   onChange={(e) => this.raiseHintChanged(e.target.value)} />
       </div>
